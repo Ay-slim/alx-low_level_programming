b43fbadbf7c1ddff008b5eb0de3252570433eb7a
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-02-21 02:46:33 -0800

    Only define functions in header file, move declaration to task files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -7,7 +8,5 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+void print_alphabet(void);
