8f427f4584ead1b2887af8f53308fc96bfe6bacd
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-02-27 07:39:43 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: String to print in reverse
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+	_putchar('\n');
+}
