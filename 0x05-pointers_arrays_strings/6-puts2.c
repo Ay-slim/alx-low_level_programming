907e2c01133c86b1d14d3c026ce119a0426c644b
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-02-27 08:51:18 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts2 - Function to print every other character in a string
+ * @str: String to print
+ * Return: Nothing
+ */
+
+void _puts2(char *str)
+{
+	int length;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		if (length % 2 == 0)
+		{
+			_putchar(str[length]);
+		}
+		length++;
+	}
+	_putchar('\n');
+}
