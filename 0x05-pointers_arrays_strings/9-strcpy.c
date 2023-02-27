791f5f3bdd671ff8ff12ea28d13f538cf9d9671c
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-02-27 09:56:53 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - Function to copy a string
+ * @src: Source string
+ * @dest: Destination string
+ * Return: A pointer to the destination string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length;
+	int counter;
+
+	counter = 0;
+	length = strlen(src);
+	while (counter < length)
+	{
+		dest[counter] = src[counter];
+		counter++;
+	}
+	return (dest);
+}
