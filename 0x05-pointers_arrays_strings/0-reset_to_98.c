5be76baedff65551ed499dd7571eec6ce9063fd6
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-02-27 05:45:23 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function to reset pointer value to 98
+ * @n: Pointer to the integer intended to change
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
