8efa96962943670ced8c93b6312166b3a866412a
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-02-27 07:43:00 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,9 +12,9 @@ void print_rev(char *s)
 	int length;
 
 	length = strlen(s);
-	while (length >= 0)
+	while (length > 0)
 	{
-		_putchar(s[length]);
+		_putchar(s[length - 1]);
 		length--;
 	}
 	_putchar('\n');
