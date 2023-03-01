49b7cbc0857cc94569064910b983ca556e17392e
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-03-01 10:38:29 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,14 +14,9 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	while (i < n)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-		if (src[i] == '\0')
-		{
-			break;
-		}
-		i++;
-	}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
