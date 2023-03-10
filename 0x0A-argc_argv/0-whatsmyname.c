44b562005d6227bc1f783eeafa3ab87a5c1d2dc0
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-03-10 01:00:04 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints arguments
+ * @argc: Argument count
+ * @argv: Arguments vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
