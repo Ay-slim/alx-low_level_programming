03ec7ce37f1643720bc5464f9b58417d32583908
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-03-10 01:04:13 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints arguments
+ * @argc: Argument count
+ * @argv: Arguments vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
