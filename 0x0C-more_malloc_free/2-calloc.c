a8199e116ed4bfaa135df6dd0f1d8840258d1c7f
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-03-16 06:30:42 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	alloc_mem = malloc(nmemb);
+	alloc_mem = malloc(nmemb * sizeof(void *));
 	if (!alloc_mem)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
