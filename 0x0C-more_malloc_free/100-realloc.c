ebb7feb01e35cff2f6d47ea4d5a24eb48270d58e
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-03-16 12:40:14 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,13 +31,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	new_ptr = malloc(new_size);
+	ptr_clone = ptr
 	if (!new_ptr)
 		return (NULL);
 
-	for (i = 0; i < old_size; i++)
-	{
-		new_ptr[i] = ptr[i];
-	}
-	free(ptr);
 	return (new_ptr);
 }
