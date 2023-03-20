95abd34a6d349bdd8b027e39619bd0aa754994d5
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-03-20 06:00:17 -0700

    dog.h

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -4,3 +4,6 @@ dog.h: Define a new type struct dog with the following elements:
 name, type = char *
 age, type = float
 owner, type = char *
+
+1-init_dog.c: Write a function that initialize a variable of type struct dog
+Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - Dog struct
  * @name: Name
  * @age: Age
  * @owner: Owner
- *
- * Description: Defines a dog struct
+ * Returns: Dog struct
  */
-
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
