0e3406959a4a3bccda24347147dcc110cc059810
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-03-20 05:45:21 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - Dog struct
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ *
+ * Description: Defines a dog struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
