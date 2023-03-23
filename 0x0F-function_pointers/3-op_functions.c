69bef6deca16f7ae75075ba6763723a39be0b246
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-03-22 17:36:48 -0700

    Helper and main functions for basic operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * op_add - Adder
+ * @a: First num
+ * @b: Second num
+ * Return: Sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Diff
+ * @a: First num
+ * @b: Second num
+ * Return: Diff
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Product
+ * @a: First num
+ * @b: Second num
+ * Return: Prod
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Div
+ * @a: First num
+ * @b: Second num
+ * Return: Div
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Mod
+ * @a: First num
+ * @b: Second num
+ * Return: Mod
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
