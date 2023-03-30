ff70b9b541d516cbfcce0c08c695fc10646ff8e4
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-03-30 07:06:07 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,28 +12,22 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *node_ptr = *head;
-	list_t *last_node_ptr;
+	list_t *node_to_add = malloc(sizeof(list_t));
 
+	if (node_to_add == NULL)
+		return (NULL);
+	node_to_add->str = strdup(str);
+	node_to_add->len = strlen(str);
+	node_to_add->next = NULL;
 	if (node_ptr == NULL)
 	{
-		node_ptr = malloc(sizeof(list_t));
-		if (node_ptr == NULL)
-			return (NULL);
-		node_ptr->str = strdup(str);
-		node_ptr->len = strlen(str);
-		node_ptr->next = NULL;
-		return (node_ptr);
+		**head = *node_to_add;
+		return (*head);
 	}
 	while (node_ptr != NULL)
 	{
 		node_ptr = node_ptr->next;
 	}
-	node_ptr->next = malloc(sizeof(list_t));
-	if (node_ptr->next == NULL)
-		return (NULL);
-	last_node_ptr = node_ptr->next;
-	last_node_ptr->str = strdup(str);
-	last_node_ptr->len = strlen(str);
-	last_node_ptr->next = NULL;
-	return (last_node_ptr);
+	node_ptr->next = node_to_add;
+	return (*head);
 }
