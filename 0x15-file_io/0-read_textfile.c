a3800562c230dde970be994bf2ba143acaec38d5
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-04-08 16:53:42 -0700

    Read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t opened_file;
+	ssize_t read_file;
+	ssize_t written_file;
+	char *bfr;
+
+	if (!filename)
+		return (0);
+
+	opened_file = open(filename, O_RDONLY);
+	if (opened_file == -1)
+		return (0);
+
+	bfr = malloc(sizeof(char) * letters);
+	if (!bfr)
+	{
+		close(opened_file);
+		return (0);
+	}
+	read_file = read(opened_file, bfr, letters);
+	if (read_file == -1)
+	{
+		free(bfr);
+		return (0);
+	}
+	written_file = write(STDOUT_FILENO, bfr, read_file);
+	if (read_file != written_file || written_file == -1)
+	{
+		free(bfr);
+		return (0);
+	}
+	return (written_file);
+}
