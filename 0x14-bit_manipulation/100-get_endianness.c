7528805f36acaee21f8071c1665f5947608950da
Ayooluwa <ayooluwaadedipe@gmail.com>
2023-04-10 13:44:43 -0700

    Get endianess

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - Gets the endianness of a system
+ * Return: 1 if little endian, 0 if big endian
+ */
+int get_endianness(void)
+{
+	unsigned int num_value = 1;
+	char *endian = (char *)&num_value;
+
+	if (*endian == 1)
+	{
+		return (1);
+	}
+	return (0);
+}
