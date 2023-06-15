7bf88c00f64f13aaad1edaad2992da62597590cf
Ay-slim <ayooluwaadedipe@gmail.com>
2023-06-15 05:14:36 -0700

    Remove trailing space

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,7 +10,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 	dlistint_t *holder = *head;
-	
+
 	if (!new)
 		return (NULL);
 	new->n = n;
